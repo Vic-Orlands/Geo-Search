9f8d14a3414496ddb513fe56da383889233b724c
Vic.Orlands <chimezieinnocent39@gmail.com>
2019-11-22 11:20:10 +0100

    Rebased my files to a new branch gh-pages for the hosting

---
+++ scripts/places.js
@@ -0,0 +1,21 @@
+
+// // Lets get a hold of the necessary Html tags here
+
+// const inputField = document.querySelector('.inputField');
+// const displayMap = document.querySelector('.mapImage');
+// const searchButton = document.querySelector('.btn');
+
+
+// const searchApi = () => {
+//     const getInputValue = inputField.value;
+//     const key = "AIzaSyB43brUjf7ZrE-vqBBAbiG3_AQ2Uxl1iTo";
+//     fetch(` https://maps.googleapis.com/maps/api/place/nearbysearch/json ?location=getInputValue&appid=${key}`)
+//     .then(res => res.json())
+//     .then(data)
+//     console.log(res)
+// }
+
+// searchButton.addEventListener('click', searchApi );
+
+// const displayWeather = document.querySelector(".weatherDisplay");
+// const facebookShare = document.querySelector(".socialPar");
