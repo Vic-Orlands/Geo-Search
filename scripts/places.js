df8a901dcb548cb11b19c764b747f145bdfd7dfc
Vic.Orlands <chimezieinnocent39@gmail.com>
2019-11-27 16:47:21 +0100

    Worked on adding the API for search autocompletion and map image

--- scripts/places.js
+++ scripts/places.js
@@ -1,11 +1,12 @@
-// Lets get a hold of the necessary Html tags here
+// --------------------------------------------------------------------Lets first get a hold of the necessary Html tags here----------------------------------------------------------------------------------
 
 const inputField = document.querySelector('.inputField');
 const displayMap = document.querySelector('.mapImage');
 const searchButton = document.querySelector('.btn');
 const displayWeather = document.querySelector(".weatherDisplay")
 
-// the proxy below allows access to the fetched openweather api else the request will be blocked by CORD
+
+//------------------------------------------------------------ the proxy below allows access to the fetched openweather api else the request will be blocked by CORD----------------------------------------------------------------
 const proxy = "https://cors-anywhere.herokuapp.com/";
 
 const searchApi = () => {
@@ -19,27 +20,27 @@ const searchApi = () => {
             <li> Pressure of ${getInputValue} is  ${data.main.pressure} </li>
             <li> Humidity of ${getInputValue} is  ${data.main.humidity} </li>
             <li> Weather of ${getInputValue} is  ${data.weather[0].main} </li>
-            <li> Wind of ${getInputValue} is  ${data.wind[0].deg}degrees </li>
             </ul>
             `
             console.log(data)
         })
-}
 
+    //-------------------------------------------------------Below is the API for the map images-------------------------------------------------------
+    const mapKey = '____';
+    const mapUrl = `https://www.mapquestapi.com/staticmap/v5/map?key=${mapKey}&locations=${getInputValue}&size=@2x&defaultMarker=marker-sm-22407F-3B5998&size=600,400@2x`;
+    fetch(mapUrl)
+        .then(res => {
+            console.log(res)
+            return displayMap.innerHTML = `<img src=${res.url} alt="Map Image" />`
+        })
+
+    inputField.value = " ";
+}
 searchButton.addEventListener('click', searchApi);
 
-// const facebookShare = document.querySelector(".socialPar");
 
-// const fetchData = async () => {
-//     try {
-//         const api = `${proxy}http://api.openweathermap.org/data/2.5/weather?q=aba&APPID=362a01de21f783b5fe2d7b810777d82e`
-//         const response = await fetch(api)
-//         const responseJSON = await response.json()
-//         console.log(responseJSON)
-//         return responseJSON
-//     } catch (err) {
-//         console.error(err)
-//     }
-// }
-
-// fetchData()
+// ------------------------------------------------------Below is the API for autocompleting word searches---------------------------------------------------------
+placeSearch({
+    key: 'mCLzct0mrLbj8On3AFJkHJPx3bv7wYme',
+    container: document.querySelector('#place-search-input')
+});
