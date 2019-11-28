7eb9a01cd77e1a6f99644fa1e4c87fc58e14f56a
Vic.Orlands <chimezieinnocent39@gmail.com>
2019-11-28 02:38:01 +0100

    Still worked on the javascript, trying to integrate the landmarks image API

--- scripts/places.js
+++ scripts/places.js
@@ -1,9 +1,9 @@
 // --------------------------------------------------------------------Lets first get a hold of the necessary Html tags here----------------------------------------------------------------------------------
-
 const inputField = document.querySelector('.inputField');
-const displayMap = document.querySelector('.mapImage');
 const searchButton = document.querySelector('.btn');
+const displayMap = document.querySelector('.mapImage');
 const displayWeather = document.querySelector(".weatherDisplay")
+const displayLandmarksImages = document.querySelector(".landmarkImageContainer")
 
 
 //------------------------------------------------------------ the proxy below allows access to the fetched openweather api else the request will be blocked by CORD----------------------------------------------------------------
@@ -14,11 +14,17 @@ const searchApi = () => {
     const api = `${proxy}http://api.openweathermap.org/data/2.5/weather?q=${getInputValue}&APPID=362a01de21f783b5fe2d7b810777d82e`
     fetch(api)
         .then(res => res.json())
-        .then(data => { 
+        .then(data => {
+            // Did the necessary celcius and fahrenheit conversion here
+            const celciusTemperature = Math.round(parseFloat(data.main.temp) - 273.15);
+            const fahrenheit = Math.round(((parseFloat(data.main.temp) - 273.15) * 1.8) + 32);
+
+            // and here, I returned the conversions
             displayWeather.innerHTML = `<ul>
-            <li> Temperature of ${getInputValue} is  ${data.main.temp} </li>
-            <li> Pressure of ${getInputValue} is  ${data.main.pressure} </li>
-            <li> Humidity of ${getInputValue} is  ${data.main.humidity} </li>
+            <li> Temperature of ${getInputValue} in Celcius is ${celciusTemperature}°C </li>
+            <li> Temperature of ${getInputValue} in Fahrenheit is  ${fahrenheit}°F </li>
+            <li> Pressure of ${getInputValue} is  ${data.main.pressure}Pa </li>
+            <li> Humidity of ${getInputValue} is  ${data.main.humidity}°C </li>
             <li> Weather of ${getInputValue} is  ${data.weather[0].main} </li>
             </ul>
             `
@@ -26,12 +32,21 @@ const searchApi = () => {
         })
 
     //-------------------------------------------------------Below is the API for the map images-------------------------------------------------------
-    const mapKey = '____';
+    const mapKey = 'mCLzct0mrLbj8On3AFJkHJPx3bv7wYme';
     const mapUrl = `https://www.mapquestapi.com/staticmap/v5/map?key=${mapKey}&locations=${getInputValue}&size=@2x&defaultMarker=marker-sm-22407F-3B5998&size=600,400@2x`;
     fetch(mapUrl)
         .then(res => {
             console.log(res)
-            return displayMap.innerHTML = `<img src=${res.url} alt="Map Image" />`
+            return displayMap.innerHTML = `<img src=${res.url} alt="Map Image of ${getInputValue}" />`
+        })
+
+    //-------------------------------------------------------Below is the API for the images-------------------------------------------------------------------------
+    const placekey = 'AIzaSyD8QCU1Lfx2_mbLNchv028xPWkB2hAIXLc'
+    const placeUrl = `${proxy}https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${getInputValue}&key=${placekey}`
+    fetch(placeUrl)
+        .then(res =>  {
+            console.log(res)
+            displayLandmarksImages.innerHTML = `<img class="landmarkImage" src="${res.url}" alt="Image of ${getInputValue}" />`
         })
 
     inputField.value = " ";
